80a7344b392ea3bd07b46991a11d1f7c5ec007c0
felix <firomerorom4@gmail.com>
2016-03-24 17:08:18 -0400

    Merge branch 'master' of https://github.com/firomero/rulemaker
    Conflicts:
    	src/AppBundle/Entity/Base_Conocimiento.php
    	src/AppBundle/Entity/Base_ConocimientoRepository.php
    	src/AppBundle/Entity/Conector.php
    	src/AppBundle/Entity/Problema.php
    	src/AppBundle/Entity/RegraConector.php
    	src/AppBundle/Entity/RegraConectorRepository.php
    	src/AppBundle/Entity/RegraFacto.php
    	src/AppBundle/Entity/Regra_producao.php
    	src/AppBundle/Manager/FactoManager.php
    	src/AppBundle/Manager/RegraConectorManager.php
    	src/AppBundle/Resources/services.yml

--- src/AppBundle/Manager/FactoManager.php
+++ src/AppBundle/Manager/FactoManager.php
@@ -14,21 +14,15 @@ use AppBundle\Entity\Facto;
 class FactoManager extends Manager
 {
    /**
-<<<<<<< HEAD
+
  * @param Facto $facto
  * @return Facto
  * @throws \Exception
  */
+
    public function SaveFacto(Facto $facto){
-      $em = $this->entityManager;
-=======
-    * @param Facto $facto
-    * @return Facto
-    * @throws \Exception
-    */
-   public function SaveFacto(Facto $facto){
+
        $em = $this->entityManager;
->>>>>>> f888585958ac5d79ea1e0c1eb7c7b46434325de6
       try{
          $em->persist($facto);
          $em->flush();
